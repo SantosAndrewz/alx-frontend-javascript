dab0fd3543ce96c9d0d54dbdd509781e6a534b1d
SantosAndrewz <sentosandrewz@gmail.com>
2024-10-08 08:16:28 +0000

    create a simple class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/*
+ * Module defines a class Classroom that accepts one attribute maxStudentsSize(number)
+ * assigned to _maxStudentsSize.
+ */
+
+export default class ClassRoom {
+  /* eslint-disable no-underscore-dangle */
+
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}

065139e47e69ac0305da13f066db3426c7179417
SantosAndrewz <sentosandrewz@gmail.com>
2025-02-09 20:01:20 +0300

    3-refactored

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -9,8 +9,8 @@
 * @returns
 */
 export default function getListStudentsIdaSum(students) {
- if (students instanceof Array) {
-   return students.reduce((sum, student) => sum + student.id, 0);
- }
- return 0;
+  if (students instanceof Array) {
+    return students.reduce((sum, student) => sum + student.id, 0);
+  }
+  return 0;
 }
